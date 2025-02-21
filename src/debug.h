0544fe7d9307e59db88adb5775697e49c88aa7b5
kriswind <126849955+kriswind@users.noreply.github.com>
2025-02-21 18:21:09 +0200

    added meson build system and disassembling chunks

---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef fantasmal_debug_h
+#define fantasmal_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
